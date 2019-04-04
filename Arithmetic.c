ef24b837bcfd9b743b183b995a35e0e0de01a0b0
Comcx <comcx@outlook.com>
2019-04-05 02:54:08 +0800

    basic

---
+++ Arithmetic.c
@@ -0,0 +1,10 @@
+#include "Arithmetic.h"
+
+
+int uaddOK(unsigned x, unsigned y) {
+
+  return x + y < x ? 0 : 1;
+}
+
+
+
---
+++ Arithmetic.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int uaddOK(unsigned x, unsigned y);
+int taddOK(int x, int y);
+
+
+
+
