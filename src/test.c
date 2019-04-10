837fbcc320964b86099bd45ac368588a94ee5578
Comcx <comcx@outlook.com>
2019-04-11 06:13:54 +0800

    log

--- src/test.c
+++ src/test.c
@@ -1,17 +1,39 @@
 
 
-
 void func() {
-
-  int a = 1;
-  if(a == 0) {
-    int b = 5;
-  }
-  else {
-    int b = 7;
+  char c = 'c';
+  int ans;
+  switch(c) {
+  case 'a':
+    ans = 'a';
+    break;
+  case 'b':
+    ans = 'b';
+    break;
+  case 'c':
+    ans = c;
+    break;
+  default:
+    break;
   }
 }
 
+void test() {
+  char c = 'c';
+  int ans;
+  if(c == 'a') {
+    ans = 'a';
+  }
+  else if(c == 'b') {
+    ans = 'b';
+  }
+  else if(c == 'c') {
+    ans = c;
+  }
+  else;
+  
+
+}
 
 
 
--- src/test.s
+++ src/test.s
@@ -10,14 +10,26 @@ func:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	movl	$1, -12(%rbp)
-	cmpl	$0, -12(%rbp)
-	jne	.L2
-	movl	$5, -8(%rbp)
-	jmp	.L4
+	movb	$99, -5(%rbp)
+	movsbl	-5(%rbp), %eax
+	cmpl	$98, %eax
+	je	.L2
+	cmpl	$99, %eax
+	je	.L3
+	cmpl	$97, %eax
+	jne	.L6
+	movl	$97, -4(%rbp)
+	jmp	.L5
 .L2:
-	movl	$7, -4(%rbp)
-.L4:
+	movl	$98, -4(%rbp)
+	jmp	.L5
+.L3:
+	movsbl	-5(%rbp), %eax
+	movl	%eax, -4(%rbp)
+	jmp	.L5
+.L6:
+	nop
+.L5:
 	nop
 	popq	%rbp
 	.cfi_def_cfa 7, 8
@@ -25,5 +37,38 @@ func:
 	.cfi_endproc
 .LFE0:
 	.size	func, .-func
+	.globl	test
+	.type	test, @function
+test:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movb	$99, -5(%rbp)
+	cmpb	$97, -5(%rbp)
+	jne	.L8
+	movl	$97, -4(%rbp)
+	jmp	.L11
+.L8:
+	cmpb	$98, -5(%rbp)
+	jne	.L10
+	movl	$98, -4(%rbp)
+	jmp	.L11
+.L10:
+	cmpb	$99, -5(%rbp)
+	jne	.L11
+	movsbl	-5(%rbp), %eax
+	movl	%eax, -4(%rbp)
+.L11:
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	test, .-test
 	.ident	"GCC: (GNU) 8.2.1 20181127"
 	.section	.note.GNU-stack,"",@progbits
